c600c8b50e193415e27075dd3b15581e6316587e
MUHAMMED ADNAN <73103641+mr-adnan-adu@users.noreply.github.com>
2025-05-29 19:44:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ADMINS = int(os.environ.get("ADMINS", "1980071557"))
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://AMK-Save-Restricted-Content:hSgvCEAHbvzTjgjl@cluster0.jrzeth7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "amksavecontentbot")
+DB_NAME = os.environ.get("DB_NAME", "AMK-Save-Restricted-Content")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
